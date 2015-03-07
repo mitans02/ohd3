0759eb0e643e581654a7c8dc8f0fecaf73929f4a
Satoshi MITANI <mitans02@ybb.ne.jp>
2015-03-08 00:48:23 +0900

    added frontpage images

--- RoboSuikawari/src/play.js
+++ RoboSuikawari/src/play.js
@@ -7,6 +7,7 @@ var playerno = 0;
 app.use('/less', express.static(__dirname + '/less'));
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
+app.use('/img', express.static(__dirname + '/img'));
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');

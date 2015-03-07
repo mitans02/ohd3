146deda06994d52ec61926610c6456218ebcf221
Satoshi MITANI <mitans02@ybb.ne.jp>
2015-03-07 23:47:45 +0900

    add toppage

--- RoboSuikawari/src/play.js
+++ RoboSuikawari/src/play.js
@@ -2,10 +2,15 @@ var express = require('express');
 var http = require('http');
 var app = express();
 var chatmsgs = new Array();
+var playerno = 0;
 
 app.use('/less', express.static(__dirname + '/less'));
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 
 app.get('/play', function (req, res) {
   res.sendfile(__dirname + '/play.html');
@@ -20,6 +25,10 @@ app.use(function(req, res, next){
     console.log("404 =>" + req.url);
 });
 
+app.get('/newplayer', function(req, res) {
+    res.send('{ "newnumber" : ' + ++playerno + ' } '); 
+});
+
 server = http.createServer(app)
 server.listen(4000);
 
@@ -27,10 +36,22 @@ var socketio = require('socket.io');
 var io = socketio.listen(server);
 
 io.sockets.on('connection', function(socket) {
+    io.sockets.emit('count', socket.client.conn.server.clientsCount);
+
+    socket.on('disconnect', function(data) {
+      console.log('disconnected');
+    });
 
     socket.on('message', function(data) {
         chatmsgs.push(data);
-        console.log(data);
+        //console.log(data);
         io.sockets.emit('message', chatmsgs);
     });
+
+    socket.on('kickstart', function(data) {
+        io.sockets.emit('start', null);
+    });
+
+
+
 });

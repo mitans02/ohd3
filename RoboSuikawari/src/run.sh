b69e7a9d37fb19ce61ff795f92966bf344c682fc
Satoshi MITANI <mitans02@ybb.ne.jp>
2015-03-08 06:48:27 +0900

    supported multiple cameras

--- RoboSuikawari/src/run.sh
+++ RoboSuikawari/src/run.sh
@@ -3,4 +3,7 @@ if [ "$MYIP" = "" ]; then
    MYIP=`ip address show eth2 | grep '192.168' | awk '{ print $2 }' | awk -F '/' '{ print $1 }'`
 fi
 sed -e "s/<%= IP %>/$MYIP/" < play.html.tmpl > play.html;
+if [ -e "/home/sysroot/usr/share/MJPEStream.marker2.js" ]; then
+  node /home/sysroot/usr/share/MJPEStream.marker2.js &
+fi
 node play.js

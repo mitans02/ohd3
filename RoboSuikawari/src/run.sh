c3d296dfa68e4ea3ddfdd0d021f37bd2144706a6
Satoshi MITANI <mitans02@ybb.ne.jp>
2015-03-07 23:47:34 +0900

    supported testenv

--- RoboSuikawari/src/run.sh
+++ RoboSuikawari/src/run.sh
@@ -1,3 +1,6 @@
-MYIP=`ip address show wlan0 | grep '192.168' | awk '{ print $2 }' | awk -F '/' '{ print $1 }'`
+MYIP=`ip address show wlan0 | grep '192.168' | awk '{ print $2 }' | awk -F '/' '{ print $1 }'  `
+if [ "$MYIP" = "" ]; then
+   MYIP=`ip address show eth2 | grep '192.168' | awk '{ print $2 }' | awk -F '/' '{ print $1 }'`
+fi
 sed -e "s/<%= IP %>/$MYIP/" < play.html.tmpl > play.html;
 node play.js
